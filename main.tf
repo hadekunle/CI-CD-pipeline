a58a07021318da6a7c919b080a8d3bf4c13852f4
hadekunle <113251943+hadekunle@users.noreply.github.com>
2022-11-26 17:28:22 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -24,7 +24,7 @@ data "aws_ami" "latest_amazon_linux" {
   }
 }
 
-resource "aws_default_vpc" "default" {} # This need to be added since AWS Provider v4.29+ to get VPC id
+# resource "aws_default_vpc" "default" {} # This need to be added since AWS Provider v4.29+ to get VPC id
 
 resource "aws_instance" "web" {
   ami                    = data.aws_ami.latest_amazon_linux.id
